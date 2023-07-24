2612aee1da8eec00ff68b5704aee25445fce9acc
Z4NTxkub <139094273+Z4NTxkub@users.noreply.github.com>
2023-07-24 10:07:54 +0700

    Add files via upload

---
+++ week4-3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{	int c ;
+	cout <<"Input C++ Score : ";
+	cin >> c;
+	cout <<"You"<<((c>=50)? "pass" : "fail")<< "with score"<< c <<endl;
+	
+	system("pause");
+	return(0);
+}
